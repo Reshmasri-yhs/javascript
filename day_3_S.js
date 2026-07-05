9a05ec3626583839e8c902ecd61563441129b22e
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-05 23:55:06 +0530

    Add files via upload

---
+++ day_3_S.js
@@ -0,0 +1,34 @@
+//JavaScript Strings
+//Create using Literals
+// Using Single Quote
+let s1 = 'abcd';
+console.log(s1);
+
+// Using Double Quote
+let s2 = "abcd";
+console.log(s2);
+
+//Create using Constructor
+let s = new String('abcd');
+console.log(s);
+
+//Template Literals (String Interpolation)
+let s1 = 'gfg';
+let s2 = `You are learning from ${s1}`;
+
+console.log(s2);
+
+//Empty String
+let s1 = '';
+let s2 = "";
+
+console.log(s1);
+console.log(s2);
+
+//Multiline Strings (ES6 and later)
+let s = `
+    This is a
+    multiline
+    string`;
+
+console.log(s);
